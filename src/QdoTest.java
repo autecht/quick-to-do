b8f0a68f341493c8d67e417e26048a832f808536
autecht <autecht@ucsd.edu>
2023-09-18 10:51:35 -0700

    Added flags for list command

--- src/QdoTest.java
+++ src/QdoTest.java
@@ -2,18 +2,23 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class QdoTest {
     String message;
     String fileName;
+    String newline;
+    String task1; String task2; String task3; String task4; String task5; String task6; String task7; String task8;
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
     @Before
     public void setupTest() {
@@ -21,6 +26,45 @@ public class QdoTest {
         this.fileName = "test.txt";
         writeMessage("test.txt", message);
     }
+
+    @Before
+    public void setupTasks() {
+        File testFile = new File("tasks.txt");
+        testFile.delete();
+
+        task1 = "RecentDate#01/12/2002#10#productivity#sldkfjlk;sdfj";
+        task2 = "LowerPriority#01/12/2002#5#productivity#sjfkhsdfjkl";
+        task3 = "NoPriority#01/12/2002###";
+        task4 = "Howdy#01/24/2002#10#productivity#lsd";
+        task5 = "Go to Dentist#09/30/2023#5#necessary#On Champlain and Perrin, 3:00pm, should pick up dinner after";
+        task6 = "Hello#09/24/2024#1##";
+        task7 = "NoDate##30##";
+        task8 = "OnlyLabel####";
+
+        writeTask(new Task(task6));
+        writeTask(new Task(task4));
+        writeTask(new Task(task1));
+        writeTask(new Task(task7));
+        writeTask(new Task(task3));
+        writeTask(new Task(task5));
+        writeTask(new Task(task8));
+        writeTask(new Task(task2));
+    }
+
+    @Before
+    public void setPrintStream() {
+        System.setOut(new PrintStream(outputStream));
+    
+    }
+
+    @Before
+    public void testOut() {
+        char lf = 13;
+        char cr = 10;
+        newline = "";
+        newline += lf; newline += cr;
+    }
+
     @Test
     public void testCompareDates() {
         int sameResult = Task.compareDates("11/22/2022", "11/22/2022");
@@ -43,11 +87,11 @@ public class QdoTest {
     @Test
     public void testCompareTask() {
         Task task1 = new Task("Task1", "02/12/2003", "5", "imp", ":SLkdjfsdlkfj");
-        String normalTask = "Feed dogs$02/29/2003$2$less$sldfkj";
+        String normalTask = "Feed dogs#02/29/2003#2#less#sldfkj";
 
-        Task similarTask = new Task("Feed doggo$02/29/2003$2$less$sldfkj");
-        String recentYear = "Feed dogo$02/29/2030$2$less$sldfkj";
-        String lowPriority = "Task$02/12/2003$0$less$sldfkj";
+        Task similarTask = new Task("Feed doggo#02/29/2003#2#less#sldfkj");
+        String recentYear = "Feed dogo#02/29/2030#2#less#sldfkj";
+        String lowPriority = "Task#02/12/2003#0#less#sldfkj";
 
         assertEquals(true, task1.compareTask(lowPriority) > 0);
         assertEquals(true, task1.compareTask(normalTask) > 0);
@@ -78,26 +122,16 @@ public class QdoTest {
         assertEquals(this.message, readFile(this.fileName));
     }
 
-    @Test
-    public void testReadLine() {
-        FileNavigator fn = new FileNavigator(this.fileName);
-        assertEquals("Line 1", fn.readLine());
-        assertEquals("Line 2", fn.readLine());
-        assertEquals("Line 3", fn.readLine());
-        assertEquals("Line 4", fn.readLine());
-        assertEquals(null, fn.readLine());
-        fn.close();
-    }
 
     @Test
     public void testGetSection() {
-        String line = "someLabel$01/12/2003$lij$$";
+        String line = "someLabel#01/12/2003#lij##";
         FileNavigator fn = new FileNavigator(this.fileName);
         String result = Task.getDate(line);
         String expected = "01/12/2003";
         assertEquals(expected, result);
 
-        line = "someLabel$$$$";
+        line = "someLabel####";
         result = Task.getDate(line);
         expected = "";
         assertEquals(expected, result);
@@ -105,28 +139,7 @@ public class QdoTest {
     }
 
     @Test
-    public void testAdd() {
-        File testFile = new File("tasks.txt");
-        testFile.delete();
-
-        String task1 = "RecentDate$01/12/2002$10$productivity$sldkfjlk;sdfj";
-        String task2 = "LowerPriority$01/12/2002$5$productivity$sjfkhsdfjkl";
-        String task3 = "NoPriority$01/12/2002$$$";
-        String task4 = "Howdy$01/24/2002$10$productivity$lsd";
-        String task5 = "Go to Dentist$09/30/2023$5$necessary$On Champlain and Perrin, 3:00pm, should pick up dinner after";
-        String task6 = "Hello$09/24/2024$1$$";
-        String task7 = "NoDate$$30$$";
-        String task8 = "OnlyLabel$$$$";
-
-        writeTask(new Task(task6));
-        writeTask(new Task(task4));
-        writeTask(new Task(task1));
-        writeTask(new Task(task7));
-        writeTask(new Task(task3));
-        writeTask(new Task(task5));
-        writeTask(new Task(task8));
-        writeTask(new Task(task2));
-        List.listAll();
+    public void testAddAndModify() {
 
         String result = readFile("tasks.txt");
         String expected = task1 + "\n" + task2 + "\n" + task3 + "\n" + task4
@@ -156,7 +169,49 @@ public class QdoTest {
         assertEquals(expected, result);
     }
 
+    @Test
+    public void testList() throws IOException {
+        setupTasks();
+        List.list("OnlyLabel", "", "", null);
+        String result = outputStream.toString();
+        String expected = "Task: OnlyLabel" + newline;
+        assertEquals(expected, result);
 
+
+        List.list("", "09/30/2024", "6", null);
+        result = outputStream.toString();
+        Task t1 = new Task(task1);
+        Task t2 = new Task(task4);
+        expected += t1.toString() + newline + t2.toString() + newline;
+        assertEquals(expected, result);
+
+        List.list("","","", "");
+        result = outputStream.toString();
+        Task t3 = new Task(task3);
+        Task t6 = new Task(task6);
+        Task t7 = new Task(task7);
+        Task t8 = new Task(task8);
+        expected += t3.toString() + newline + t6.toString() 
+                + newline + t7.toString() + newline + t8.toString() + newline;
+        assertEquals(expected, result);
+        /*
+        task1 = "RecentDate#01/12/2002#10#productivity#sldkfjlk;sdfj";
+        task2 = "LowerPriority#01/12/2002#5#productivity#sjfkhsdfjkl";
+        task3 = "NoPriority#01/12/2002###";
+        task4 = "Howdy#01/24/2002#10#productivity#lsd";
+        task5 = "Go to Dentist#09/30/2023#5#necessary#On Champlain and Perrin, 3:00pm, should pick up dinner after";
+        task6 = "Hello#09/24/2024#1##";
+        task7 = "NoDate##30##";
+        task8 = "OnlyLabel####";
+         */
+        
+    }
+
+    @After
+    public void close() throws IOException {
+        outputStream.close();
+    }
+    
     /*
      * Puts contents of file with path fileName into String
      * Assumes file ends in newline
